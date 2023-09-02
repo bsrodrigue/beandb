972122b29cac869a6cc137542adbec3bbb4e1588
rodo <brodrigue@protonmail.co>
2023-09-02 17:03:48 +0000

    feat(compiler): start building compiler

--- src/main.c
+++ src/main.c
@@ -3,14 +3,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
-  char* buffer;
+typedef struct
+{
+  char *buffer;
   size_t buffer_length;
   ssize_t input_length;
 } InputBuffer;
 
-InputBuffer* new_input_buffer(){
-  InputBuffer* input_buffer = (InputBuffer*) malloc(sizeof(InputBuffer));
+InputBuffer *new_input_buffer()
+{
+  InputBuffer *input_buffer = (InputBuffer *)malloc(sizeof(InputBuffer));
 
   input_buffer->buffer = NULL;
   input_buffer->buffer_length = 0;
@@ -19,50 +21,70 @@ InputBuffer* new_input_buffer(){
   return input_buffer;
 }
 
-void print_prompt() {
+void print_prompt()
+{
   printf("db > ");
 }
 
-void read_input(InputBuffer* input_buffer){
+void read_input(InputBuffer *input_buffer)
+{
   ssize_t bytes_read = getline(
-    &(input_buffer->buffer),
-    &(input_buffer->buffer_length),
-    stdin
-  );
+      &(input_buffer->buffer),
+      &(input_buffer->buffer_length),
+      stdin);
 
-
-  if (bytes_read <= 0){
+  if (bytes_read <= 0)
+  {
     printf("Error reading input\n");
     exit(EXIT_FAILURE);
   }
 
-  // Ignore trailing newline bruh
   input_buffer->input_length = bytes_read - 1;
   input_buffer->buffer[input_buffer->input_length] = 0;
 }
 
-void close_input_buffer(InputBuffer* input_buffer){
+void close_input_buffer(InputBuffer *input_buffer)
+{
   free(input_buffer->buffer);
   free(input_buffer);
 }
 
 int main(int argc, char *argv[])
 {
-  InputBuffer* input_buffer = new_input_buffer(); 
+  InputBuffer *input_buffer = new_input_buffer();
 
-  while (1) {
-    print_prompt(); 
+  while (1)
+  {
+    print_prompt();
     read_input(input_buffer);
 
-    if(strcmp(input_buffer->buffer, ".exit") == 0){
-      close_input_buffer(input_buffer);
-      exit(EXIT_SUCCESS);
+    if (input_buffer->buffer[0] == '.')
+    {
+      switch (do_meta_command(input_buffer))
+      {
+      case (META_COMMAND_SUCCESS):
+        continue;
+
+      case (META_COMMAND_UNRECOGNIZED_COMMAND):
+        printf("Unrecognized command '%s'\n", input_buffer->buffer);
+        continue;
+      }
     }
 
-    else {
-      printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+    Statement statement;
+
+    switch (prepare_statement(input_buffer, &statement))
+    {
+    case (PREPARE_SUCCESS):
+      break;
+
+    case (PREPARE_UNRECOGNIZED_STATEMENT):
+      printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
+      continue;
     }
 
+    execute_statement(&statement);
+    printf("Executed.\n");
   }
 
   return EXIT_SUCCESS;
